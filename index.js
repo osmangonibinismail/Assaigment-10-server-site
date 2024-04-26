c476607162c62352ef47f44e7cdc7402823338e5
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-04-26 21:38:46 +0600

    express added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) =>{
+    res.send('Assaigment 10 is running')
+})
+
+app.listen(port, () => {
+    console.log(`Assaigment 10 is running on port: ${port}`)
+})
