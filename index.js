51e5d8586f04b54e4083d7ab04b9c95f51423209
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-04-30 22:03:18 +0600

    strict added

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
-    // version: ServerApiVersion.v1,
+    version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   }
