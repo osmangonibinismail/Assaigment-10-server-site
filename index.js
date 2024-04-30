95c0a6d426d7436cc9e0c4ec4c0ef1959d161bcb
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-04-30 23:48:16 +0600

    all info updated

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ async function run() {
         res.send(result);
     })
 
+    
     app.delete('/add/:id', async (req, res) =>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
